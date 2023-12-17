a6eca7f2c13a01cea56d52e870fc44bc80aaae76
slqnt <94934670+slqntdevss@users.noreply.github.com>
2023-12-17 03:00:17 +0000

    hotfix 2

--- static/index.js
+++ static/index.js
@@ -1,36 +1,18 @@
-"use strict";
-/**
- * @type {HTMLFormElement}
- */
-const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
-const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("uv-error-code");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
-  location.href = "/uv.html?url=" + url;
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = input.value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "http://" + url;
+      sessionStorage.setItem("url", __uv$config.encodeUrl(url));
+      location.href = "go";
+    });
 });
--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,8 @@
-importScripts('/mathhelp/bundle.js');
-importScripts('/mathhelp/config.js');
-importScripts(__uv$config.sw || '/mathhelp/sw.js');
+importScripts("/mathhelp/bundle.js");
+importScripts("/mathhelp/config.js");
+importScripts("/mathhelp/sw.js");
 
 const sw = new UVServiceWorker();
+let userKey = new URL(location).searchParams.get('userkey');
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
