0d80940d570e4f6dff17879c70e56396358c3176
slqnt <94934670+slqntdevss@users.noreply.github.com>
2023-12-17 23:35:08 +0000

    test failed :pensive:

--- static/mathhelp/config.js
+++ static/mathhelp/config.js
@@ -1,16 +1,7 @@
-const barechosen = "/contactus/"
-const barelist = []
-barelist[0] = "/contactus/"
-barelist[1] = "https://bare2.mysticmath.workers.dev/"
-
-var bRand = 0;
-bRand = Math.random();
-bRand = Math.floor(bRand * barelist.length);
-barechosen = barelist[bRand]
 
 self.__uv$config = {
   prefix: "/versat/",
-  bare: barechosen,
+  bare: "https://bare2.mysticmath.workers.dev/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/mathhelp/handler.js",
